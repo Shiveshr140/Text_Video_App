153c232a2e17ec09fe46eafc38d15a616f92ba88
apple <apple@apples-MacBook-Pro-5.local>
2025-12-13 14:37:17 +0530

    new tunnel url

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import StatusIndicator from './components/StatusIndicator';
 import VideoPlayer from './components/VideoPlayer';
 import './App.css';
 
-const API_BASE = "https://aged-idle-assembly-tries.trycloudflare.com";
+const API_BASE = "https://inflation-digit-agree-glass.trycloudflare.com";
 
 const videoTypes = [
   {
