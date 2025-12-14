ac63f63389e2fd71cf7d2a54364ac3d93b38033e
apple <apple@apples-MacBook-Pro-5.local>
2025-12-14 22:31:56 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import VideoPlayer from './components/VideoPlayer';
 import './App.css';
 
 
-const API_BASE = "https://equality-employment-turn-folding.trycloudflare.com"
+const API_BASE = "https://sphere-dried-pets-works.trycloudflare.com"
 const videoTypes = [
   {
     id: 'text',
