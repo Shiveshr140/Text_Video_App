90ad2dab8a0b7b37074b236126b1a97c2e1980e8
apple <apple@apples-MacBook-Pro-5.local>
2025-12-25 13:31:19 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import VideoPlayer from './components/VideoPlayer';
 import './App.css';
 
 
-const API_BASE = "https://tomatoes-submitting-drainage-boating.trycloudflare.com"
+const API_BASE = "https://forgot-rays-troops-glasses.trycloudflare.com"
 const videoTypes = [
   {
     id: 'text',
