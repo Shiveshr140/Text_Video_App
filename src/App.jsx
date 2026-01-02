f705b8be4c0ba38ae48470956eccc279317875fe
apple <apple@apples-MacBook-Pro-5.local>
2026-01-02 12:07:01 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import StatusIndicator from './components/StatusIndicator';
 import VideoPlayer from './components/VideoPlayer';
 import './App.css';
 
-const API_BASE = "https://journalists-mate-dietary-expired.trycloudflare.com"
+const API_BASE = "https://molecules-charitable-cooking-detector.trycloudflare.com"
 const videoTypes = [
   {
     id: 'text',
