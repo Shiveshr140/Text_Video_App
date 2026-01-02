e5ea537dc72264d6feec7d6a830a7461a6f36468
apple <apple@ip-192-168-1-38.ap-south-1.compute.internal>
2026-01-02 23:07:53 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import StatusIndicator from './components/StatusIndicator';
 import VideoPlayer from './components/VideoPlayer';
 import './App.css';
 
-const API_BASE = "https://molecules-charitable-cooking-detector.trycloudflare.com"
+const API_BASE = "https://digest-tons-larger-shareholders.trycloudflare.com"
 const videoTypes = [
   {
     id: 'text',
