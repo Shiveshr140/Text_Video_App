782abadb7c28c53c09cc1ccb399b9704bd00e6cf
apple <apple@apples-MacBook-Pro-5.local>
2026-01-30 15:47:42 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import StatusIndicator from './components/StatusIndicator';
 import VideoPlayer from './components/VideoPlayer';
 import './App.css';
 
-const API_BASE = "https://variations-certainly-mineral-nick.trycloudflare.com"
+const API_BASE = "https://introduce-boat-mood-income.trycloudflare.com"
 const videoTypes = [
   {
     id: 'text',
