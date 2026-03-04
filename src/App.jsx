2069fc42b724f73a975655087d85343b6199c083
apple <apple@apples-MacBook-Pro-5.local>
2026-03-04 23:01:43 +0530

    updated url

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import StatusIndicator from './components/StatusIndicator';
 import VideoPlayer from './components/VideoPlayer';
 import './App.css';
 
-const API_BASE = "https://coupled-keyboard-timeline-sort.trycloudflare.com"
+const API_BASE = "https://mate-wife-supplied-cancer.trycloudflare.com"
 
 const supportedLanguages = [
   { key: 'english', code: 'en-IN', label: 'English' },
