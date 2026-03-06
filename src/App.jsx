db973b2c567eeeea6b954d28ff0fcfeb6331f5b0
apple <apple@apples-MacBook-Pro-5.local>
2026-03-06 23:55:11 +0530

    updated url

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import StatusIndicator from './components/StatusIndicator';
 import VideoPlayer from './components/VideoPlayer';
 import './App.css';
 
-const API_BASE = "https://sale-midlands-browsers-mistakes.trycloudflare.com"
+const API_BASE = "https://shareholders-cubic-cardiac-spokesman.trycloudflare.com"
 
 
 const supportedLanguages = [
