a3874a48f7184f9c87e5910f579f9720ddf492be
apple <apple@apples-MacBook-Pro-5.local>
2026-03-08 18:43:50 +0530

    update url

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import StatusIndicator from './components/StatusIndicator';
 import VideoPlayer from './components/VideoPlayer';
 import './App.css';
 
-const API_BASE = "https://shareholders-cubic-cardiac-spokesman.trycloudflare.com"
+const API_BASE = "https://martha-concerned-thumbnails-from.trycloudflare.com"
 
 
 const supportedLanguages = [
